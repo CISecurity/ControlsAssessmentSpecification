a7090f2d287cdafbf25b57a0e4972f64f6f4797c
pswens <134083635+pswens@users.noreply.github.com>
2023-12-07 15:14:21 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,11 @@
+version: 2
 
+python:
+  #install:
+    #requirements: docs/requirements.txt
+    #method: pip
+    #  path: .
+    #  extra_requirements:
+    #   - docs
+    - method: pip
+      path: another/package
